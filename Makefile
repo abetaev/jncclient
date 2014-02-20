071567310c60a00620bc4fd3cc469c22c3b39f22
Alexander Betaev <abetaev@griddynamics.com>
2014-02-21 00:09:27 +0400

    fix wrapper

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ all: ncsvc-wrapper.so
 
 CFLAGS = -fPIC -std=c99 -m32
 LDFLAGS = -shared -m32
-LIBS=-L/usr/lib32 -ldl -lc_nonshared -lm
+LIBS=-L/usr/lib32 -ldl -lc -lm
 
 ncsvc-wrapper.so: ncsvc-wrapper.o
 	$(CC) $(LDFLAGS) -o $@ $^ $(LIBS)
--- ncsvc-wrapper.c
+++ ncsvc-wrapper.c
@@ -10,9 +10,10 @@
 #include <unistd.h>
 
 
-static int (* real_open)(const char *, int);
-static int (* real_chown)(const char *, uid_t, gid_t);
-static int (* real_stat)(const char *, struct stat64 *);
+static int (* open_next)(const char *, int);
+static int (* chown_next)(const char *, uid_t, gid_t);
+static int (* __xstat_next)(int ver, const char *, struct stat *);
+static int (* __fxstat_next)(int ver, int, struct stat *);
 
 static int (* real_main)(int, char **);
 
@@ -32,45 +33,45 @@ const char * process_path(const char *path) {
 }
 
 int open(const char *pathname, int flags) {
-    return real_open(process_path(pathname), flags);
+    return open_next(process_path(pathname), flags);
 }
 
 int chown(const char *path, uid_t owner, gid_t group) {
 	if (strstr(path, ".juniper_networks") != NULL) {
 		return 0;
 	}
-    return real_chown(path, owner, group);
+    return chown_next(path, owner, group);
 }
 
-int stat64(const char *pathname, struct stat64 *buf) {
-	if (real_stat == NULL) {
-		real_stat = dlsym(RTLD_NEXT, "stat64");
-    	if (real_stat == NULL) {
-        	puts("Failed to dynamicly link int (* stat64)(const char * pathname, struct stat *buf)");
-	        exit(3);
-    	}
-	}
-	puts("calling real_stat");
-	return real_stat(process_path(pathname), buf);
+int __xstat(int ver, const char *pathname, struct stat *buf) {
+	return __xstat_next(ver, process_path(pathname), buf);
+}
+
+int __fxstat(int ver, int fb, struct stat *buf) {
+	return __fxstat_next(ver, fb, buf);
 }
 
 void init() {
-    real_open = dlsym(RTLD_NEXT, "open");
-    if (real_open == NULL) {
-        puts("Failed to dynamicly link int (* open)(const char * pathname, int flags)");
+    open_next = dlsym(RTLD_NEXT, "open");
+    if (open_next == NULL) {
+        puts("Failed to dynamicly link open");
         exit(3);
     }
-    real_chown = dlsym(RTLD_NEXT, "chown");
-    if (real_chown == NULL) {
-        puts("Failed to dynamicly link int (* chown)(const char * path, uid_t owner, git_t group)");
+    chown_next = dlsym(RTLD_NEXT, "chown");
+    if (chown_next == NULL) {
+        puts("Failed to dynamicly link chown");
         exit(3);
     }
-	real_stat = dlsym(RTLD_NEXT, "stat64");
-    if (real_stat == NULL) {
-        puts("Failed to dynamicly link int (* stat)(const char * pathname, struct stat *buf)");
+	__xstat_next = dlsym(RTLD_NEXT, "__xstat");
+    if (__xstat_next == NULL) {
+        puts("Failed to dynamicly link __xstat");
         exit(3);
     }
-
+	__fxstat_next = dlsym(RTLD_NEXT, "__fxstat");
+    if (__fxstat_next == NULL) {
+        puts("Failed to dynamicly link __fxstat");
+        exit(3);
+    }
     jnc_client_log_path = "/root/jncclient.log";
 }
 
